40d87058a40a514cbd6d61bf4222df36e84cd117
stlfinestnae <36461588+stlfinestnae@users.noreply.github.com>
2019-02-22 17:13:31 -0600

    update

--- Program.cs
+++ Program.cs
@@ -1,91 +1,103 @@
 using System;
+using System.Collections.Generic;
 
 namespace URLEncoder
 {
     class Program
     {
-        static string UrlTemp = "https://companyserver.com/content/[project_name]/files/[activity_name]/[activity_name]Report.pdf";
-
-
-
+        static string urlFormat = "https://companyserver.com/content/{0}/files/{1}/{1}Report.pdf";
+        //MAIN PROGRAM   
         static void Main(string[] args)
         {
-            firststep:
-                Console.WriteLine("NAEK47 CUSTOM URL GENERATOR\n");
-                Console.WriteLine("Wut is da project name:\n");
-                string PN = Console.ReadLine();
-                if (IsValid(PN) is false)
-                    {
-                        goto firststep;
-                    }
-                    PN = CheckInput(PN);
-
-            nextstep:
-                Console.WriteLine("Please enter an activity name:\n");
-                string AN = Console.ReadLine();
-                if (IsValid(AN) is false)
-                    {
-                        goto nextstep;
-                    }
-                    AN = CheckInput(AN);
-
-                    Console.WriteLine(CustomURLMaker(PN, AN));
-
-            finalstep:
-                    Console.WriteLine("Want another NAEK Custom URL? (yes/no) ");
-                    string respond = Console.ReadLine();
-
-            switch (respond)
-                    {
-                        case "yes":
-                                goto firststep;
-                        case "no":
-                                break;
-                        default:
-                                Console.WriteLine("Not an option. Try Again.\n");
-                                goto finalstep;
-                    }
-        }
-
-                static string CustomURLMaker(string PN, string AN)
-                 {
-                    return String.Format(UrlTemp, PN, AN);
-                 }
-        
-        
-         static string CheckInput(string input)
-        {
-            string[] illegal = new string[] { " ", "/", "?", ":", "@", ";", "&", "=", "+", "$", ",", "{", "}", "|", "\\", "^", "[", "]", "`" };
-            string[] replacement = new string[] { "%20", "%2F", "%3F", "%40", "%3B", "%26", "%3D", "%2B","%24", "%2C", "%7B", "%7D", "%7C", "%5C", "%5E", "%5B", "%5D", "%60" };
-
-            foreach (string element in illegal)
+            console.WriteLine("NAEK47 CUSTOM URL GENERATOR\n");
+            
+            do
             {
-                int index = Array.IndexOf(illegal, element);
-
-                if (input.Contains(element))
-                        {
-                            input = input.Replace(element, replacement[index]);
-                        }
-            }
-            return String.Format(input);
+                console.Write("Enter project name: \n");
+                    string projectName = GetUserInput();
+                
+                console.Write("Enter activity name: \n");
+                    string activityName = GetUserInput();
+                
+                console.WriteLine(createURL(projectName,activityName));
+                
+                console.Write("Want another NAEK47 CUSTUM URL GENERATOR? (y/n): ");
+            } while (Console.ReadLine().ToLower().Equals("y"));
         }
         
-
-        static bool IsValid(string input)
+        
+        //FUNCTIONS
+        static string GetUserInput()                             
         {
-            foreach (char character in input)
-            {
-                if (character <= 0x1F || character == 0x7F)
+	        string input = "";
+	            do
+	              {   
+		            input = Console.ReadLine();
+		            if (IsValid(input)) return input;
+		            Console.Write("The input contains invalid characters. Enter again: ");
+	              } while (true);
+        }
+        
+        static bool IsValid(string input){
+            
+            foreach (char character in input.ToCharArray()){
+                
+                if(character >= 0x00 || character <= 0x1F)
                 {
-                    Console.WriteLine("ERROR. Cannot Use Control Character. Try Again.");
+                    console.WriteLine("Can't use control characters. Try another option:\n");
                     return false;
                 }
+                else if(character == 0x7F)
+                {
+                    console.WriteLine("Can't use control characters. Try another option:\n");
+                    return false;
+                }
+              
             }
             return true;
         }
-
-
-
+        
+        static string Encode(string value)
+        {
+            string EncodedValue = "";
+            
+            foreach (char character in value)
+            {
+                value = value.Replace(" ", "%20");
+                value = value.Replace("<", "%3C");
+                value = value.Replace(">", "%3E");
+                value = value.Replace("#", "%23");
+                value = value.Replace("%", "%25");
+                value = value.Replace(""", "%22");
+                value = value.Replace(";", "%3B");
+                value = value.Replace("/", "%2F");
+                value = value.Replace("?", "%3F");
+                value = value.Replace(":", "%3A");
+                value = value.Replace("@", "%40");
+                value = value.Replace("&", "%26");
+                value = value.Replace("=", "%3D");
+                value = value.Replace("+", "%2B");
+                value = value.Replace("$", "%24");
+                value = value.Replace(",", "%2C");
+                value = value.Replace("{", "%7B");
+                value = value.Replace("}", "%7D");
+                value = value.Replace("|", "%7C");
+                value = value.Replace("\", "%5C");
+                value = value.Replace("^", "%5E");
+                value = value.Replace("[", "%5B");
+                value = value.Replace("]", "%5D");
+                value = value.Replace("`", "%60");
+                                      
+                EncodedValue = value;
+            }                                    
+            return EncodedValue;
+        }
+                                      
+        static string CreateURL(string projectName, string activityName) {
+            
+            return String.Format(urlFormat, projectName, activityName);
+        }
+        
     }
-    
 }
+
