f8a0b901bdcaed8f7813682f3a909ddc5525d896
stlfinestnae <36461588+stlfinestnae@users.noreply.github.com>
2019-02-22 01:17:40 -0600

    update

--- Program.cs
+++ Program.cs
@@ -10,8 +10,13 @@ using System;
                 string project_name = "";
                 string project_activity = "";
                 
-                console.WriteLine("Enter da project name:");
-                project_name = Console.ReadLine();
+                a:
+                    console.WriteLine("Enter da project name:");
+                    project_name = Console.ReadLine();
+                    if(FirstCheck(project_name) == 1){
+                        goto a;
+                    }
+                    
                 
                 
                 
@@ -19,24 +24,18 @@ using System;
         }
     }
 
-        public int FirstCheck(string FirstCheck)
+        static bool FirstCheck(string FirstCheck)
         {
-            if (FirstCheck == "%00"|| "%01"|| "%02"|| "%03"|| "%04"|| "%05"|| "%06"|| "%07"|| "%08"|| "%07"|| "%08"|| "%09"){
-                
-                console.WriteLine("Not valid, Dat's a control character.");
-                return 1;
+          foreach (char character in FirstCheck)
+            {
+                if (character <= 0x1F || character == 0x7F)
+                {
+                    Console.WriteLine("ERROR. Contains a control character, try again.");
+                    return 1;
             }
-            else if (FirstCheck == "%0A"|| "%0B"|| "%0C"|| "%0D"|| "%0E"|| "%0F"|| "%1A"|| "%1B"|| "%1C"|| "%1D"|| "%1E"|| "%1F"){
-                
-                console.WriteLine("Not valid, Dat's a control character.");
-                return 1;
-            }
-            else if (FirstCheck == "%10"|| "%11"|| "%12"|| "%13"|| "%14"|| "%15"|| "%16"|| "%17"|| "%18"|| "%19"){
-                
-                console.WriteLine("Not valid, Dat's a control character.");
-                return 1;
-            }
-            else{
-                return 0;
-            }
+                else{
+                    return 0;
+                }
         }
+
+        
