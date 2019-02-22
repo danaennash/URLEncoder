0f630082cad3252e273ef940f45224e6624fafac
stlfinestnae <36461588+stlfinestnae@users.noreply.github.com>
2019-02-22 03:32:01 -0600

    update

--- Program.cs
+++ Program.cs
@@ -4,84 +4,84 @@ namespace URLEncoder
 {
     class Program
     {
-        static string UrlFormat = "https://companyserver.com/content/{0}/files/{1}/{1}Report.pdf";
+        static string UrlTemp = "https://companyserver.com/content/[project_name]/files/[activity_name]/[activity_name]Report.pdf"
+
+
 
         static void Main(string[] args)
         {
-            a:
-            Console.WriteLine("Please enter a project name:\n");
-            string project_name = Console.ReadLine();
-            if (IsValid(project_name) is false)
+            firststep:
+                Console.WriteLine("NAEK47 CUSTOM URL GENERATOR\n");
+                Console.WriteLine("Wut is da project name:\n");
+                string PN = Console.ReadLine();
+                if (IsValid(PN) is 1)
             {
-                goto a;
+                goto firststep;
             }
-            project_name = CheckInput(project_name);
+            PN = CheckInput(PN);
 
-            b:
+            nextstep:
             Console.WriteLine("Please enter an activity name:\n");
-            string activity_name = Console.ReadLine();
-            if (IsValid(activity_name) is false)
+            string AN = Console.ReadLine();
+            if (IsValid(AN) is 1)
             {
-                goto b;
+                goto nextstep;
             }
-            activity_name = CheckInput(activity_name);
+            AN = CheckInput(AN);
 
-            Console.WriteLine(CreateUrl(project_name, activity_name));
+            Console.WriteLine(CreateUrl(PN, AN));
 
-            c:
-            Console.WriteLine("Would you like to create another URL? (Y/N) ");
+            finalstep:
+            Console.WriteLine("Want another NAEK Custom URL? (yes/no) ");
             string answer = Console.ReadLine();
 
             switch (answer)
             {
-                case "Y":
-                    goto a;
-                case "N":
+                case "yes":
+                    goto firststep;
+                case "no":
                     break;
                 default:
-                    Console.WriteLine("You made an invalid choice.\n");
-                    goto c;
+                    Console.WriteLine("Not an option. Try Again.\n");
+                    goto finalstep;
             }
         }
 
-
+                static string CustomURLMaker(string PN, string AN)
+                 {
+                    return String.Format(Urltemp, PN, AN);
+                 }
+        
         static bool IsValid(string input)
         {
             foreach (char character in input)
             {
                 if (character <= 0x1F || character == 0x7F)
                 {
-                    Console.WriteLine("Invalid input. Contains a control character. ");
-                    return false;
+                    Console.WriteLine("ERROR. Cannot Use Control Character. Try Again.");
+                    return 1;
                 }
             }
-            return true;
+            return 0;
         }
 
 
         static string CheckInput(string input)
         {
-            string[] illegal = new string[] { "%", " ", "<", ">", "#", "\"", ";", "/", "?", ":", "@", "&", "=", "+", "$", ",", "{", "}", "|", "\\", "^", "[", "]", "`" };
-            string[] replacement = new string[] { "%25", "%20", "%3C", "%3E", "%23", "%22", "%3B", "%2F", "%3F", "%3A", "%40", "%26", "%3D", "%2B", "%24", "%2C", "%7B", "%7D", "%7C", "%5C", "%5E", "%5B", "%5D", "%60" };
+            string[] badLetter = new string[] { "%", " ", "<", ">", "#", "\"", ";", "/", "?", ":", "@", "&", "=", "+", "$", ",", "{", "}", "|", "\\", "^", "[", "]", "`" };
+            string[] replace = new string[] { "%25", "%20", "%3C", "%3E", "%23", "%22", "%3B", "%2F", "%3F", "%3A", "%40", "%26", "%3D", "%2B", "%24", "%2C", "%7B", "%7D", "%7C", "%5C", "%5E", "%5B", "%5D", "%60" };
 
-            foreach (string element in illegal)
+            foreach (string element in badLetter)
             {
-                int index = Array.IndexOf(illegal, element);
+                int i = Array.IndexOf(badLetter, element);
 
                 if (input.Contains(element))
                 {
-                    input = input.Replace(element, replacement[index]);
+                    input = input.Replace(badLetter, replace[i]);
                 }
             }
             return String.Format(input);
         }
-
-
-        static string CreateUrl(string project_name, string activity_name)
-        {
-            return String.Format(UrlFormat, project_name, activity_name);
-        }
-
 
     }
 }
