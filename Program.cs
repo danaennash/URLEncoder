5a3f1135ce6af8e2a1e69a3a2d84f2de3ddd1537
stlfinestnae <36461588+stlfinestnae@users.noreply.github.com>
2019-02-21 22:20:32 -0600

    update

--- Program.cs
+++ Program.cs
@@ -2,8 +2,19 @@ using System;
 
     namespace URLEncoder
     {
+        
         class Program
         {
-        
+            static void Main(string[] args){
+                
+                console.WriteLine("NAEK47 URL GENERATOR");
+                string project_name = "";
+                string project_activity = "";
+                
+                console.WriteLine("Enter da project name:");
+                prroject_name = Console.ReadLine();
+                
+   
+            }
         }
     }
