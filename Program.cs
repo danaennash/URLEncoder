a5f77bb918d2cd0b6e8d79887b048c7deb185851
stlfinestnae <36461588+stlfinestnae@users.noreply.github.com>
2019-02-22 04:35:25 -0600

    update

--- Program.cs
+++ Program.cs
@@ -15,58 +15,43 @@ namespace URLEncoder
                 Console.WriteLine("Wut is da project name:\n");
                 string PN = Console.ReadLine();
                 if (IsValid(PN) is false)
-            {
-                goto firststep;
-            }
-            PN = CheckInput(PN);
+                    {
+                        goto firststep;
+                    }
+                    PN = CheckInput(PN);
 
             nextstep:
-            Console.WriteLine("Please enter an activity name:\n");
-            string AN = Console.ReadLine();
-            if (IsValid(AN) is false)
-            {
-                goto nextstep;
-            }
-            AN = CheckInput(AN);
+                Console.WriteLine("Please enter an activity name:\n");
+                string AN = Console.ReadLine();
+                if (IsValid(AN) is false)
+                    {
+                        goto nextstep;
+                    }
+                    AN = CheckInput(AN);
 
-            Console.WriteLine(CustomURLMaker(PN, AN));
+                    Console.WriteLine(CustomURLMaker(PN, AN));
 
             finalstep:
-            Console.WriteLine("Want another NAEK Custom URL? (yes/no) ");
-            string answer = Console.ReadLine();
+                    Console.WriteLine("Want another NAEK Custom URL? (yes/no) ");
+                    string respond = Console.ReadLine();
 
-            switch (answer)
-            {
-                case "yes":
-                    goto firststep;
-                case "no":
-                    break;
-                default:
-                    Console.WriteLine("Not an option. Try Again.\n");
-                    goto finalstep;
-            }
+            switch (respond)
+                    {
+                        case "yes":
+                        goto firststep;
+                        case "no":
+                        break;
+                        default:
+                            Console.WriteLine("Not an option. Try Again.\n");
+                            goto finalstep;
+                    }
         }
 
                 static string CustomURLMaker(string PN, string AN)
                  {
                     return String.Format(UrlTemp, PN, AN);
                  }
-        
-        static bool IsValid(string input)
-        {
-            foreach (char character in input)
-            {
-                if (character <= 0x1F || character == 0x7F)
-                {
-                    Console.WriteLine("ERROR. Cannot Use Control Character. Try Again.");
-                    return false;
-                }
-            }
-            return true;
-        }
-
-
-       static string CheckInput(string input)
+               static string CheckInput(string input)
         {
             string[] illegal = new string[] { " ", "/", "?", ":", "@", ";", "&", "=", "+", "$", ",", "{", "}", "|", "\\", "^", "[", "]", "`" };
             string[] replacement = new string[] { "%20", "%2F", "%3F", "%40", "%3B", "%26", "%3D", "%2B","%24", "%2C", "%7B", "%7D", "%7C", "%5C", "%5E", "%5B", "%5D", "%60" };
@@ -82,6 +67,21 @@ namespace URLEncoder
             }
             return String.Format(input);
         }
+
+        static bool IsValid(string input)
+        {
+            foreach (char character in input)
+            {
+                if (character <= 0x1F || character == 0x7F)
+                {
+                    Console.WriteLine("ERROR. Cannot Use Control Character. Try Again.");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
 
     }
     
